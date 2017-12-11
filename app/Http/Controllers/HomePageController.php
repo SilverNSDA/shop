6609e140dd8a1d41aacf0f508b89afb4e3ed17ec
SilverNSDA <hatoriNSDA@gmail.com>
2017-12-11 15:47:42 +0700

    layout update

---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomePageController extends Controller
+{
+    public function home(){
+        return view('HomePage');
+    }
+    public function shop(){
+        return view('Shop');
+    }
+    
+}
